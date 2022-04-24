e49f15eca6f8d1832830ee6a982bca4c16090b75
Ilkhom Dodov <dodlordisback@gmail.com>
2022-04-24 19:50:21 +0300

    Annotation changed

--- src/test/java/ru/netology/sqr/SquareCalculatorTest.java
+++ src/test/java/ru/netology/sqr/SquareCalculatorTest.java
@@ -1,6 +1,7 @@
 package ru.netology.sqr;
 
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -8,10 +9,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class SquareCalculatorTest {
 
     @ParameterizedTest
-    @CsvSource(value = {"'squares counted, bottom border included',100,144,3",
-            "'squares counted, top border included',9409,9801,3",
-            "'no squares in range included',1,99,0",
-            "'max amount of squares in range',100,9801,90"})
+    @CsvFileSource(resources = "/data.csv")
     void calculate(String testName, int floor, int ceiling, int expected) {
         SquareCalculator service = new SquareCalculator();
 
